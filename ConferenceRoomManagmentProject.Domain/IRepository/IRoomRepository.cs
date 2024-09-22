42326698de41a9bbd1f3ab6565480c8ed5cf75c2
Ihor Ziubin <zyubin.igor@gmail.com>
2024-09-22 20:32:38 +0300

    add IRoomRepository and branch check

---
+++ ConferenceRoomManagmentProject.Domain/IRepository/IRoomRepository.cs
@@ -0,0 +1,11 @@
+﻿using ConferenceRoomManagmentProject.Domain.Entities;
+namespace ConferenceRoomManagmentProject.Domain.IRepository;
+
+public interface IRoomRepository
+{
+    Room GetRoomById(int id);
+    IEnumerable<Room> GetAvailableRooms(DateTime startTime, DateTime endTime, int capacity);
+    void AddRoom(Room room);
+    void UpdateRoom(Room room);
+    void DeleteRoom(int id);
+}
