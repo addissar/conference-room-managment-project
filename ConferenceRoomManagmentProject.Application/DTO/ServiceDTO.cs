eb8b56e5e723f33666a5dff961daffa2a90e957c
Ihor Ziubin <zyubin.igor@gmail.com>
2024-09-25 04:13:23 +0300

    add Pricing Rules and DTO entities(next branch)

---
+++ ConferenceRoomManagmentProject.Application/DTO/ServiceDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace ConferenceRoomManagmentProject.Application.DTO;
+
+public class ServiceDTO
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ ConferenceRoomManagmentProject.Domain/IEntities/IPricingService.cs
@@ -0,0 +1,8 @@
+﻿using ConferenceRoomManagmentProject.Domain.Entities;
+
+namespace ConferenceRoomManagmentProject.Domain.IEntities;
+
+public interface IPricingService
+{
+    decimal CalculateBookingCost(Room room, DateTime startTime, DateTime endTime, List<Service> services);
+}
